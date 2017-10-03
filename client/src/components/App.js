8ed9592651323d4832a003b4ee2465c0828bd127
Jimmy Lin <usmcjimmy@gmail.com>
2017-10-03 13:45:06 -0400

    refactored Landing into a seperate folder as component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
-import Landing from './Landing';
+import Landing from './landing/Landing';
 
 class App extends Component {
   render() {
