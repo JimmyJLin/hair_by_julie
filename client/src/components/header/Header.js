32949b1076de256104bd9b2ced5d2f860561ab9f
Jimmy Lin <usmcjimmy@gmail.com>
2017-10-12 09:16:57 -0400

    removed bootstrap and react-bootstrap

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Link } from 'react-router-dom';
 
 import './_header.scss'
 
@@ -9,53 +6,7 @@ class Header extends Component {
   render() {
     return (
       <div id="header" className="container-fluid">
-        <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to={'/'}>React-Bootstrap</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <LinkContainer to={'/'}>
-                <NavItem eventKey={1} href="#">Link</NavItem>
-              </LinkContainer>
-              <LinkContainer to={'/'}>
-                <NavItem eventKey={2} href="#">Link</NavItem>
-              </LinkContainer>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-
-                <LinkContainer to={'/'}>
-                  <MenuItem eventKey={3.1}>Action</MenuItem>
-                </LinkContainer>
-                <LinkContainer to={'/'}>
-                  <MenuItem eventKey={3.2}>Another action</MenuItem>
-                </LinkContainer>
-                <LinkContainer to={'/'}>
-                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                </LinkContainer>
-
-                <MenuItem divider />
-
-                <LinkContainer to={'/'}>
-                  <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </LinkContainer>
-
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <LinkContainer to={'/'}>
-                <NavItem eventKey={1} href="#">Link Right</NavItem>
-              </LinkContainer>
-
-              <LinkContainer to={'/'}>
-                <NavItem eventKey={2} href="#">Link Right</NavItem>
-              </LinkContainer>
-
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <h2>Navbar</h2>
       </div>
     )
   }
