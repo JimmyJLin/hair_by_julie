dcccf0a3fddf6c320d67c20f9128575bc9bbf45d
Jimmy Lin <usmcjimmy@gmail.com>
2017-10-12 10:24:06 -0400

    setup basic header, footer, services, about, reviews, contact, and appointment components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
 import Landing from './landing/Landing';
+import Services from './services/Services';
+import About from './about/About';
+import Reviews from './reviews/Reviews';
+import Contact from './contact/Contact';
+import Appointment from './appointment/Appointment';
 import Header from './header/Header';
 import Footer from './footer/Footer';
 
@@ -13,6 +18,11 @@ class App extends Component {
         <div>
           <Header />
           <Route exact path="/" component={Landing}/>
+          <Route exact path="/services" component={Services}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/reviews" component={Reviews}/>
+          <Route exact path="/contact" component={Contact}/>
+          <Route exact path="/appointment" component={Appointment}/>
           <Footer />
         </div>
       </BrowserRouter>
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './_header.scss'
 
 class Header extends Component {
   render() {
     return (
-      <div id="header" className="container-fluid">
-        <h2>Navbar</h2>
+      <div id="header" className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper">
+            <Link to={'/'} className="brand-logo">Logo</Link>
+            <Link to={'/'} data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></Link>
+            <ul className="right hide-on-med-and-down">
+              <li><Link to={'/services'}>Services</Link></li>
+              <li><Link to={'/about'}>About</Link></li>
+              <li><Link to={'/reviews'}>Reviews</Link></li>
+              <li><Link to={'/contact'}>Contact</Link></li>
+              <li><Link to={'/appointment'}>Appointment</Link></li>
+
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><Link to={'/services'}>Services</Link></li>
+              <li><Link to={'/about'}>About</Link></li>
+              <li><Link to={'/reviews'}>Reviews</Link></li>
+              <li><Link to={'/contact'}>Contact</Link></li>
+              <li><Link to={'/appointment'}>Appointment</Link></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     )
   }
