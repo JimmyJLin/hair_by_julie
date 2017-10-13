410e2dc8f482be79add39679a5dc1eeb0347f7cb
Jimmy Lin <usmcjimmy@gmail.com>
2017-10-13 12:19:14 -0400

    added slider, and landing page ipsum layouts

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import './_landing.scss'
 
+import imageOne from './hair.jpg'
+
 import Slider from '../slider/Slider';
+import ContactMe from '../utils/ContactMe';
 
 class Landing extends Component {
   render() {
     return (
-      <div id="landing" className="container-fluid">
+      <div id="landing">
         <Slider />
-        <h1>
-          Hello World!
-        </h1>
-        <p>
-          Welcome to Node Express React Landing Page!
-        </p>
+
+        <div id="about" className="container">
+          <h1 id="title">Content Title</h1>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+        </div>
+        <div id="image_content">
+          <img src={imageOne} alt=""/>
+        </div>
+
+        <div id="address" className="container">
+          <h4>Hair By Julie</h4>
+          <h5>123 5th Avenue 2nd Floor New York , NY 10003</h5>
+          <h5>Tel: 917-386-5485</h5>
+        </div>
+
+        <ContactMe />
+
       </div>
     );
   }
