ec2a92157936991b6880a11d4c9fc617f62cf0f6
Jimmy Lin <usmcjimmy@gmail.com>
2017-10-13 00:19:39 -0400

    added fixed bottom menu icons

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './_footer.scss'
 
@@ -6,6 +7,20 @@ class Footer extends Component {
   render() {
     return (
       <footer id="footer" className="page-footer">
+
+        <div className="fixed-action-btn vertical">
+          <Link to={'/'} className="btn-floating btn-large red">
+            <i className="large material-icons">mode_edit</i>
+          </Link>
+          <ul>
+            <li><Link to={'/services'} className="btn-floating red"><i className="material-icons">menu</i></Link></li>
+            <li><Link to={'/about'} className="btn-floating yellow darken-1"><i className="material-icons">face</i></Link></li>
+            <li><Link to={'/reviews'} className="btn-floating green"><i className="material-icons">stars</i></Link></li>
+            <li><Link to={'/contact'} className="btn-floating red"><i className="material-icons">call</i></Link></li>
+            <li><Link to={'/appointment'} className="btn-floating blue"><i className="material-icons">schedule</i></Link></li>
+          </ul>
+        </div>
+
         <div className="container">
           <div className="row">
             <div className="col l6 s12">
